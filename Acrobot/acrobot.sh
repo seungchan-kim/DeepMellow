4185a5d9c83385259902d53514a240a7cb3bd95e
skim25 <seungchan_kim@brown.edu>
2019-10-07 18:48:54 -0400

    Update acrobot.sh

--- Acrobot/acrobot.sh
+++ Acrobot/acrobot.sh
@@ -6,10 +6,9 @@
 for i in {1..100}
 do
     /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 max $i
-	/users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 mellow 1 $i
+    /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 mellow 1 $i
     /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 mellow 2 $i
     /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 mellow 5 $i
-    /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf1 mellow 10 $i
     /users/sk99/myenv/bin/python3 -u acrobot.py param/lr1e-3_h3_nn300_tuf100 max $i
 done
 
